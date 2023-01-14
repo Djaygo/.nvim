7aefdbf9ae9de964c653dcd0610bf08584176240
Diego Staphorst <D.B.Staphorst@protonmail.com>
2023-01-14 14:26:46 +0100

    feat: added some extra features

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(
+        vim.list_extend(opts.sources, { { name = "emoji" }, { name = "copilot", group_index = 2 } })
+      )
+    end,
+  },
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "black",
+        "isort",
+      },
+    },
+  },
+}
