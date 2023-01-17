908bd50a75845a0ba7d62a2e8b2bdc29ccdb453f
Diego Staphorst <D.B.Staphorst@protonmail.com>
2023-01-17 23:04:20 +0100

    refactor: move example plugins to seperate files

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,9 +5,8 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.sources = cmp.config.sources(
-        vim.list_extend(opts.sources, { { name = "emoji" }, { name = "copilot", group_index = 2 } })
-      )
+      opts.sources =
+        cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" }, { name = "copilot", group_index = 2 } }))
     end,
   },
   -- add pyright to lspconfig
@@ -23,6 +22,40 @@ return {
     },
   },
   {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      local nls = require("null-ls")
+      nls.setup({
+        debounce = 150,
+        save_after_format = false,
+        sources = {
+          -- nls.builtins.formatting.prettierd,
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.fish_indent,
+          -- nls.builtins.formatting.fixjson.with({ filetypes = { "jsonc" } }),
+          -- nls.builtins.formatting.eslint_d,
+          -- nls.builtins.diagnostics.shellcheck,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.diagnostics.markdownlint,
+          -- nls.builtins.diagnostics.luacheck,
+          nls.builtins.formatting.prettierd.with({
+            filetypes = { "markdown" }, -- only runs `deno fmt` for markdown
+          }),
+          nls.builtins.diagnostics.selene.with({
+            condition = function(utils)
+              return utils.root_has_file({ "selene.toml" })
+            end,
+          }),
+          -- nls.builtins.code_actions.gitsigns,
+          nls.builtins.formatting.isort,
+          nls.builtins.formatting.black,
+          nls.builtins.diagnostics.flake8,
+        },
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", ".git"),
+      })
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
@@ -32,6 +65,7 @@ return {
         "flake8",
         "black",
         "isort",
+        "sumneko_lua",
       },
     },
   },
