181d22ac16d2871690fa208b0385a259feac969e
Diego Staphorst <D.B.Staphorst@protonmail.com>
2023-01-17 23:04:20 +0100

    refactor: move example plugins to seperate files

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,8 +30,11 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight", "habamax", "gruvbox" } },
   checker = { enabled = true }, -- automatically check for plugin updates
+  opts = {
+    colorscheme = "gruvbox",
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
