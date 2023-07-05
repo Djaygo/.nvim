4442be0d22a47e17082b2a3c2b96820f787479d5
Diego Staphorst <D.B.Staphorst@protonmail.com>
2023-07-05 21:50:28 +0200

    fix: more

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,19 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
+
+    { import = "lazyvim.plugins.extras.editor.flash" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
+    -- import/override with your plugins
+    { import = "plugins" },
+    -- import any extras modules here
+
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -30,11 +43,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax", "gruvbox", "sourcerer" } },
+  install = { colorscheme = { "tokyonight", "habamax", "gruvbox" } },
   checker = { enabled = true }, -- automatically check for plugin updates
-  opts = {
-    colorscheme = "sourcerer",
-  },
   performance = {
     rtp = {
       -- disable some rtp plugins
