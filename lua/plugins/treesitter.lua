3dca461c39591275664e9729a9784e659d36ed3f
Diego Staphorst <D.B.Staphorst@protonmail.com>
2023-12-03 21:05:46 +0100

    fix: no idea actually

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,11 @@
 return {
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    highlight = { enable = true },
+    indent = { enable = true },
     opts = {
       ensure_installed = {
         "bash",
@@ -10,16 +13,33 @@ return {
         "javascript",
         "json",
         "toml",
+        "yaml",
         "lua",
         "markdown",
         "markdown_inline",
         "python",
         "query",
         "regex",
-        "tsx",
-        "typescript",
         "vim",
         "yaml",
+      },
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
       },
     },
   },
